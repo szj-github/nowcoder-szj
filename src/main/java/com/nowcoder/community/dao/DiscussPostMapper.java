ccfe8c61b3ce0d7f9eb562a4968ac7caf0d8ff29
sunzhijian <2541809243@qq.com>
2023-11-07 18:09:36 +0800

    test2

--- src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -8,8 +8,8 @@ import java.util.List;
 @Mapper
 public interface DiscussPostMapper {
     List<DiscussPost> selectDiscussPosts(int userId,int offset,int limit);//分页查询
-    //一共有多少行
-    //如果只有一个参数，并且在<if>语句里使用，则必须加别名
+    /*    一共有多少行
+    如果只有一个参数，并且在<if>语句里使用，则必须加别名*/
     int selectDiscussPostRows(@Param("userId") int userId);
 
 
